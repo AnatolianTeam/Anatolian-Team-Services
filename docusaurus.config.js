213105dba15fb6cbd53671f33da80908b9949660
Mehmet Köl Tiğin <102043225+koltigin@users.noreply.github.com>
2024-03-23 03:41:42 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://AnatolianTeam.github.io',
+  url: 'https://services.anatolianteam.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
